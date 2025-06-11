3a60e1d87e0e370b188dabb740f244a48e0fb94c
Sarvesh-10 <sarveshsugandh92@gmail.com>
2025-06-11 19:45:43 +0530

    added logging

--- app/middleware.py
+++ app/middleware.py
@@ -2,26 +2,29 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import Request, HTTPException
 import jwt
 import os
+import logging
 
 class JWTAuthMiddleware(BaseHTTPMiddleware):
     '''Middleware for JWT Authentication'''
 
     async def dispatch(self, request, call_next):
-        print("HERE in middleware")
+        logging.info(f"Processing request: {request.method} {request.url.path}")
         if request.url.path.startswith('/api'):
-            print("HERE")
+            logging.info("Request is for API endpoint, checking JWT token...")
             # Try reading the token from the cookie
             print(request.cookies.items())
             token = request.cookies.get('token')
-            print("HERE", token)
+            logging.info(f"Received token: {token}")  # Log the received token
 
             if not token:
                 raise HTTPException(status_code=401, detail='Authorization token missing')
 
             try:
                 payload = jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=['HS256'])
-                print("payload is ",payload)
+                
+                logging.info(f"Decoded JWT payload: {payload}")
                 request.state.user_id = payload['user_id']
+                logging.info(f"User ID from token: {request.state.user_id}")
             except jwt.ExpiredSignatureError:
                 raise HTTPException(status_code=401, detail='Token expired')
             except jwt.InvalidTokenError:
